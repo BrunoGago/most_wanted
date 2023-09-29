92f22b52ef3065466b478c7df84c4067f5e79e86
Bruno Gago <bruno.gago@ab-inbev.com>
2023-09-28 21:01:24 -0300

    first commit

---
+++ src/test/java/com/fiap/idwall/most_wanted/MostWantedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.idwall.most_wanted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostWantedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
