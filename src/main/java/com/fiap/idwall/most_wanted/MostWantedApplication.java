3fd35c49387dbf84e4ce7f69f9526b428d64b262
Yago Silva <yago.mdk284@gmail.com>
2023-09-30 17:13:52 -0300

    Included project

--- src/main/java/com/fiap/idwall/most_wanted/MostWantedApplication.java
+++ src/main/java/com/fiap/idwall/most_wanted/MostWantedApplication.java
@@ -1,13 +1,5 @@
 package com.fiap.idwall.most_wanted;
 
-import java.io.IOException;
-
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,34 +9,39 @@ public class MostWantedApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MostWantedApplication.class, args);
 
-		String url = "https://www.fbi.gov/wanted";
+		// String url = "https://www.fbi.gov/wanted/terrorism";
 
-		try {
+		// try {
+		// // Conectar à página da web e definir o User-Agent
+		// Connection connection = Jsoup.connect(url)
+		// .userAgent(
+		// "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like
+		// Gecko) Chrome/105.0.5454.0 Safari/537.36 Edg/105.0.5454.0");
 
-			// Definindo User-Agent para não tomar block da página
-			Connection connection = Jsoup.connect(url)
-					.userAgent(
-							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5454.0 Safari/537.36 Edg/117.0.2045.47");
+		// // Obter o conteúdo HTML
+		// Document document = connection.get();
 
-			Document document = connection.get();
-			// Esse dá block na página com HTTO 403, falando que não tenho autorização,
-			// metodo
-			// Jsoup.connect(url).get();
+		// // Encontrar os elementos desejados na página usando seletores CSS
+		// Elements wantedItems = document.select(".portal-type-person");
 
-			// Encontre os elementos desejados na página usando seletores CSS
-			Elements wantedItems = document.select(".portal-type-person");
+		// // Iterar pelos elementos e extrair informações
+		// for (Element item : wantedItems) {
+		// String crime = item.select(".title").text();
+		// String name = item.select(".name").text();
+		// String detailsUrl = item.select("a").first().attr("href");
 
-			// Iterar pelos elementos e extrair informações
-			for (Element item : wantedItems) {
-				String name = item.select(".title").text();
-				String detailsUrl = "https://www.fbi.gov" + item.select(".title a").attr("href");
+		// Element image = item.select("img").first();
+		// String imageUrl = image.attr("src");
 
-				System.out.println("Nome: " + name);
-				System.out.println("URL de Detalhes: " + detailsUrl);
-				System.out.println("--------------------------------------");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// System.out.println("Crime: " + crime);
+		// System.out.println("Nome: " + name);
+		// System.out.println("URL de Detalhes: " + detailsUrl);
+		// System.out.println("URL da Imagem: " + imageUrl);
+
+		// System.out.println("--------------------------------------");
+		// }
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
 	}
 }
--- src/main/java/com/fiap/idwall/most_wanted/models/Wanted.java
+++ src/main/java/com/fiap/idwall/most_wanted/models/Wanted.java
@@ -1,38 +1,38 @@
 package com.fiap.idwall.most_wanted.models;
 
-import java.util.Date;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Entity(name = "wanted")
-@Table(name = "TB_WANTED")
+@Entity(name = "wanted_fbi")
+@Table(name = "TB_WANTED_FBI")
 @Data
 public class Wanted {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID_WANTED", length = 9, nullable = false, unique = true)
+    @Column(name = "ID", length = 9, nullable = false, unique = true)
     private Long id;
+
+    @Column(name = "CRIME", length = 55, nullable = false)
+    private String crime;
 
     @Column(name = "FULL_NAME", length = 55, nullable = false)
     private String fullName;
 
-    @Column(name = "BIRTH_DATE", nullable = false)
-    private Date birthDate;
+    @Column(name = "DETAILS_URL", length = 100, nullable = false)
+    private String detailsUrl;
 
-    @Column(name = "GENDER", length = 1, nullable = false)
-    private char gender;
+    @Column(name = "IMAGE_URL", length = 100, nullable = false)
+    private String imageUrl;
 
-    @OneToMany(mappedBy = "wanted", cascade = CascadeType.ALL)
-    private List<Wanted_Crime> wantedCrimes;
-    
+    public Wanted(String crime, String name, String detailsUrl, String imageUrl) {
+        this.crime = crime;
+        this.fullName = name;
+        this.detailsUrl = detailsUrl;
+        this.imageUrl = imageUrl;
+    }
 }
